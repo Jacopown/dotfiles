72e8a1ae9fa755f247fbe660a5a899e403b04d41
Jacopown <jacopomehi@gmail.com>
2022-05-28 18:24:30 +0000

    tab uniformation

--- awesome/.config/awesome/error_handler.lua
+++ awesome/.config/awesome/error_handler.lua
@@ -4,9 +4,11 @@ local naughty = require("naughty")
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
+    naughty.notify({ 
+        preset = naughty.config.presets.critical,
+        title = "Oops, there were errors during startup!",
+        text = awesome.startup_errors 
+    })
 end
 
 -- Handle runtime errors after startup
@@ -16,10 +18,11 @@ do
         -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
+        naughty.notify({ 
+            preset = naughty.config.presets.critical,
+            title = "Oops, an error happened!",
+            text = tostring(err) 
+        })
         in_error = false
     end)
 end
