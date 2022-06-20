d3e743dc5f7e78f22658fceb5123848e100308a7
Jacopown <jacopomehi@gmail.com>
2022-06-20 14:19:54 +0000

    first implementation of textclock widget

--- awesome/.config/awesome/widgets/textclock.lua
+++ awesome/.config/awesome/widgets/textclock.lua
@@ -1,4 +1,29 @@
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+local gears = require("gears")
 local wibox = require("wibox")
 
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock()
+textclock = wibox.widget{
+    widget = wibox.container.background,
+    --bg = "#ff0000",
+    shape  = gears.shape.rounded_rect,
+    {
+        widget = wibox.container.margin,
+        margins = dpi(5),
+        {
+            widget = wibox.container.background,
+            bg = "#504945",
+            shape  = gears.shape.rounded_rect,
+            {
+                widget = wibox.container.margin,
+                margins = dpi(5),
+                {
+                    layout = wibox.layout.align.horizontal,
+                    wibox.widget.textclock("%a %d %B, %H:%M")
+                }
+            }     
+        }    
+    }
+}
+
