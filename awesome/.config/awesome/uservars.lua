65db79207448851d3e804fd25f350e3042fdc4e8
Jacopown <jacopomehi@gmail.com>
2022-06-23 13:44:36 +0000

    compiling uservars array

--- awesome/.config/awesome/uservars.lua
+++ awesome/.config/awesome/uservars.lua
@@ -2,14 +2,18 @@ local beautiful = require("beautiful")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
+uservars = {
+    terminal = "alacritty",
+    bar_height = dpi(40)
+}
+
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init("/home/username/.config/awesome/themes/myFirstTheme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
 editor = "nvim"
-editor_cmd = terminal .. " -e " .. editor
+editor_cmd = uservars.terminal .. " -e " .. editor
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
@@ -17,7 +21,3 @@ editor_cmd = terminal .. " -e " .. editor
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
-
-uservars = {
-    
-}
