f6fdc9353ecfb94cb4d0e62ddec888c97976cf49
Jacopown <jacopomehi@gmail.com>
2022-06-25 19:48:11 +0000

    added picom to autostart

--- awesome/.config/awesome/autostart.lua
+++ awesome/.config/awesome/autostart.lua
@@ -3,4 +3,5 @@ local awful = require("awful")
 --{{{Autostart
 awful.spawn.with_shell("nitrogen --set-scaled ~/dotfiles/wallpapers/devillevitate.jpg &")
 awful.spawn.with_shell("lxsession &")
+awful.spawn.with_shell("picom &")
 ---}}}
