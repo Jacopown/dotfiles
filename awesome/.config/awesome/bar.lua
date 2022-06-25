7d16a5618c8ce2cd863029ccfd9ebf974ff4364b
Jacopown <jacopomehi@gmail.com>
2022-06-25 17:24:38 +0000

    first wip bar

--- awesome/.config/awesome/bar.lua
+++ awesome/.config/awesome/bar.lua
@@ -8,16 +8,15 @@ local wibox = require("wibox")
 awful.screen.connect_for_each_screen(function(s)
     s.bar = awful.popup{
         screen = s,
-        ontop = false,
+        type = "dock",
         widget = wibox.container.background,
-        --bg = "#ffffff",
         placement = function(c) awful.placement.top(c, {margins = 3}) end,
-        maximum_width = dpi(1910),
-        maximum_height = uservars.bar_height,
-        visible = true,
+        maximum_width = s.geometry.width - 10,
+        minimum_width = s.geometry.width - 10,
+        maximum_height = uservars.bar_height, 
         shape = function(cr, width, height)
             gears.shape.rounded_rect(cr, width, height, 10)
-        end
+        end        
     }
 
     s.bar:struts {
@@ -26,17 +25,21 @@ awful.screen.connect_for_each_screen(function(s)
 
     s.bar:setup {
         layout = wibox.layout.align.horizontal,
+        expand="none",
         {
             layout = wibox.layout.align.horizontal,
             mainmenu,
             s.taglist,
         },
-        s.tasklist,
         {
             layout = wibox.layout.align.horizontal,
-            s.systray,
+            s.tasklist,
+        },
+        {
+            layout = wibox.layout.align.horizontal,
+            wibox.widget.systray(),
             textclock,
             s.layoutbox
-        },
-      }
+        }
+    }
 end)
