d2e8fdcb56a1c313b9e41842a63950c1f38fb06a
Jacopown <jacopomehi@gmail.com>
2022-08-10 23:38:51 +0200

    changed close buffer command and added lsp as diagnostics source

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -6,9 +6,10 @@ end
 bufferline.setup {
   options = {
     mode = "buffers",
-    close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
     offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "center", padding = 1 } },
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+    diagnostics = "nvim_lsp",
   },
 }
