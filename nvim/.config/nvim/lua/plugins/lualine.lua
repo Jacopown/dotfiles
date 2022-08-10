0ebf7f0ae38be874b50d85d0e7abf2920aef0192
Jacopown <jacopomehi@gmail.com>
2022-08-10 23:39:41 +0200

    minor changes

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,54 +1,54 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
 
 local hide_in_width = function()
-  return vim.fn.winwidth(0) > 80
+	return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-  "diagnostics",
-  sources = { "nvim_diagnostic" },
-  sections = { "error", "warn" },
-  symbols = { error = " ", warn = " " },
-  colored = ture,
-  always_visible = true,
+	"diagnostics",
+	sources = { "nvim_diagnostic", "nvim_lsp" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = true,
+	always_visible = true,
 }
 
 local diff = {
-  "diff",
-  colored = true,
-  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width,
+	"diff",
+	colored = true,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	cond = hide_in_width,
 }
 
 local filetype = {
-  "filetype",
-  icons_enabled = true,
+	"filetype",
+	icons_enabled = true,
 }
 
 local location = {
-  "location",
-  padding = 0,
+	"location",
+	padding = 0,
 }
 
-lualine.setup {
-  options = {
-    globalstatus = true,
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = { "alpha", "dashboard" },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = {"branch"},
-    lualine_c = { diagnostics },
-    lualine_x = { diff },
-    lualine_y = { filetype },
-    lualine_z = { location },
-  },
-}
+lualine.setup({
+	options = {
+		globalstatus = true,
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { diff, "branch" },
+		lualine_c = { diagnostics },
+		lualine_x = { },
+		lualine_y = { filetype },
+		lualine_z = { location },
+	},
+})
