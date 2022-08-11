6b263fdaaa89d8ad4422a8d6ef74dca1021ed26d
Jacopown <jacopomehi@gmail.com>
2022-08-11 13:49:45 +0200

    solved problems with autopairs

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,8 +1,13 @@
--- Setup nvim-cmp.
 local status_ok, npairs = pcall(require, "nvim-autopairs")
 if not status_ok then
   return
 end
+
+local rule_status_ok, Rule = pcall(require, "nvim-autopairs.rule")
+if not rule_status_ok then
+  return
+end
+
 
 npairs.setup {
   check_ts = true, -- treesitter integration
@@ -10,4 +15,12 @@ npairs.setup {
   fast_wrap = {
     map = '<C-e>',
   },
+}
+
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}', '""',"''" }, pair)
+    end),
 }
