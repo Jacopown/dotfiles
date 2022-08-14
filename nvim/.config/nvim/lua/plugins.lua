15e4bfe019f0d5443281227f228b5df78260acbc
Jacopown <jacopomehi@gmail.com>
2022-08-14 21:27:30 +0200

    norcalli version of nvim-colorizer is no more mantained, switched with NvChad fork

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -74,7 +74,7 @@ return packer.startup(function(use)
 	-- BHO --
 	use({ "akinsho/toggleterm.nvim", commit = "53c9d50add7c0afd563ed7a6e221422a693f625b" })
 	use({ "lewis6991/gitsigns.nvim", commit = "9c3ca027661136a618c82275427746e481c84a4e" })
-	use({ "norcalli/nvim-colorizer.lua", commit = "36c610a9717cc9ec426a07c8e6bf3b3abcb139d6" })
+	use({ "NvChad/nvim-colorizer.lua", commit = "3e7c1e0e8048d37d8e5b711d9fa005d25b64d54a" })
   use { "ahmedkhalf/project.nvim", commit = "090bb11ee7eb76ebb9d0be1c6060eac4f69a240f"}
 
 	-- cmp plugins
