95ba58c48a9ecad4e06faa56f91d8f6be08cb16c
Jacopown <jacopomehi@gmail.com>
2022-12-24 09:44:14 +0100

    moved neovim config in its own repo

--- nvim/.config/nvim/lua/colorscheme.lua
+++
@@ -1,6 +0,0 @@
-local colorscheme = "nord"
-
-local colorscheme_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not colorscheme_ok then
-	vim.notify("There was a problem while requiring colorscheme")
-end
